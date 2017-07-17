0e1039c91e8ae011c3c27705fb9bb17c0611b30f
salvobabani <salvatorbabani@gmail.com>
2017-07-17 15:09:24 +0300

    guvenlik libs üzerinde çalışıldı.

---
+++ libs/guvenlik.go
@@ -0,0 +1,49 @@
+package libs
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/salvobabani92/salesperformans.com/config"
+	"net/http"
+	"github.com/salvobabani92/salesperformans.com/models"
+)
+
+func BiletKontrol(c *gin.Context) {
+	var tokenStr string = c.Request.Header.Get(config.TokenHeaderName)
+
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return []byte(config.MySecretKey), nil
+	})
+	if err == nil && token.Valid {
+		var user = models.User{}
+		config.DB.First(&user, int64(ParseToken(tokenStr, "id").(float64)))
+		if user.ID == 0 {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "Lütfen tekrar sisteme giriş yapınız."})
+			c.Abort()
+		}
+		c.Next()
+	} else {
+		c.JSON(http.StatusUnauthorized, gin.H{"status": "Lütfen tekrar sisteme giriş yapınız."})
+		c.Abort()
+	}
+}
+
+// context içinde tanımlı olan kullanıcı bilgilerine ve firma bilgilerine erişim sağlanır.
+func GetUser_Company(c *gin.Context) (models.User, models.Company) {
+	var tokenStr string = c.Request.Header.Get(config.TokenHeaderName)
+
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return []byte(config.MySecretKey), nil
+	})
+	if err == nil && token.Valid {
+		var user = models.User{}
+		config.DB.First(&user, int64(ParseToken(tokenStr, "id").(float64)))
+
+		var company = models.Company{}
+		config.DB.First(&company, user.CompanyID)
+		return user, company
+
+	} else {
+		return models.User{}, models.Company{}
+	}
+}
