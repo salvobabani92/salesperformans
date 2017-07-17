322ad9119817004bcc62297a4ace1deb4e16e50c
salvobabani <salvatorbabani@gmail.com>
2017-07-17 14:05:06 +0300

    ping controller üzerinde çalışıldı.

--- controller/ping.go
+++ controller/ping.go
@@ -1,1 +1,14 @@
+
+
 package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"github.com/salvobabani92/salesperformans.com/models"
+)
+
+func Ping(c *gin.Context) {
+	c.JSON(http.StatusOK, models.Pong{Message: "pong..."})
+}
+
