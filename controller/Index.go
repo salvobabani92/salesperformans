666f1d262fdc28ad00e92764ef77239216bb7e95
salvobabani <salvatorbabani@gmail.com>
2017-07-17 14:03:56 +0300

    index controller  üzerinde çalışıldı.

--- controller/Index.go
+++ controller/Index.go
@@ -1,1 +1,17 @@
+
+
 package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func  Index(c *gin.Context) {
+
+	c.HTML(http.StatusOK, "index", gin.H{
+		"user":   "q",
+		"userID": "1",
+		"Title":"Hello",
+	})
+}
