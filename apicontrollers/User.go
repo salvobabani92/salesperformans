7c357a1e2f7891f93fee229211bf5ebd895050fc
salvobabani <salvatorbabani@gmail.com>
2017-07-19 13:47:27 +0300

    User apicontroller üzerinde çalışıldı.

--- apicontrollers/User.go
+++ apicontrollers/User.go
@@ -1,1 +1,12 @@
 package apicontrollers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/salvobabani92/salesperformans.com/libs"
+	"net/http"
+)
+
+func GetMe(c *gin.Context) {
+	user, _ := libs.GetUser_Company(c)
+	c.JSON(http.StatusOK, user)
+}
