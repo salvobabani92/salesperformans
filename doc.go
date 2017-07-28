aea74c2410bfef33185339633ff1a9a0c8de248a
salvobabani <salvatorbabani@gmail.com>
2017-07-28 18:45:12 +0300

    doc.go  üzerinde çalışıldı.

---
+++ doc.go
@@ -0,0 +1,17 @@
+// Sales Performans
+// Api dökümantasyonu aşağıda çıkartılmıştır.
+// Schemes: http, https
+// Host: localhost
+// BasePath: /
+// Version: 0.0.1
+// License: MIT http://opensource.org/licenses/MIT
+// Consumes:
+//   - application/json
+//   - application/xml
+//
+// Produces:
+//   - application/json
+//   - application/xml
+//
+// swagger:meta
+package main
